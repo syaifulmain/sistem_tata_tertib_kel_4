1062a32cd8b7e950f572b8c301bc83ebdc37c532
MaulanaHamzah <144118963+MaulanaHamzah@users.noreply.github.com>
2024-11-18 11:26:57 +0700

    hapus lapor

--- app/View/mahasiswa/lapormhs.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Tombol Tambah Laporan
-    document.getElementById('tambah-laporan').addEventListener('click', () => {
-        window.location.href = 'tambah_laporan.php';
-    });
-
-    // Event listeners atau interaksi JavaScript tambahan dapat ditambahkan di sini
-});
